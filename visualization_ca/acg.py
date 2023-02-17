66c840279e7b5a353eeb8ca738cd51f7e728f043
zm711 <92116279+zm711@users.noreply.github.com>
2023-02-17 11:21:47 -0500

    Update acg.py
    fixed variable names from n and x from before. Changed bin size to be closer to normal sample rates, deleted the rust portions of the code. Fixed conditional logic since I changed bin size.. Also switched to spike counts rather than Hz

--- visualization_ca/acg.py
+++ visualization_ca/acg.py
@@ -15,14 +15,9 @@ OUTPUTS: graphs of ACGs for each cluster in sp
 """
 
 import numpy as np
-import histdiff as hd
+from histdiff import histdiff
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-try:
-    import ordhist
-except ModuleNotFoundError:
-    print("no rust code available")
 
 
 def plotACGs(sp: dict, refract_time=0.002) -> None:
@@ -44,48 +39,44 @@ def plotACGs(sp: dict, refract_time=0.002) -> None:
 
 def acg(st: np.array, sample_rate: float, cluster: int, ref_per: float) -> None:
 
-    n_spikes = len(st)
-    bin_size = 0.00002  # do small bins to get accurate sizes
-    acg_bins = np.arange(1 / sample_rate / 2, 0.2, bin_size)  # generate the bins
-    bins2 = acg_bins.copy()  # copy for using the rust version of code
-    try:
-        ordhist.bincenterpy(bins2)  # generates bin centers
-        hd.rusthist(st, st, acg_bins)  # generates the counts
-        x2 = bins2[:-1]  # set our x-axis delete extra column
-        number2 = acg_bins[:-1]  # set our counts and delte one extra column
-        number2 = number2 / bin_size / n_spikes  # conversion from Nick's code
-    except NameError:  # if no rust default back to python implementation
-        print("Using python algo")
-        n2, x2 = hd.histdiff(st, st, acg_bins)
-        number2 = n2 / bin_size / n_spikes
+    # n_spikes = len(st)
+    bin_size = 0.00025  # do small bins to get accurate sizes
+    # n_bins = int(np.ceil((0.2- (1/(2*sample_rate)))/bin_size))
+    # acg_bins = np.linspace(1/(2*sample_rate), 0.2, n_bins)
+    # need a couple samples after 0 to 200 milliseconds with small bins for time
+    acg_bins = np.arange(1 / (sample_rate * 2), 0.2, bin_size)  # generate the bins
+
+    spike_counts, bin_centers = histdiff(st, st, acg_bins)
 
     """We check if we have enough spikes in our small window to actually show up
     if not we use the whole 200 ms. If we have enough we look at only 20 ms of data.
     People are allowed to put in their own refractory period. I default to 2 ms."""
 
-    if sum(number2[:1001]) < 200:
-        x2_vals = np.concatenate((-np.flip(x2), x2))
-        stairs_val = np.concatenate((np.flip(number2), number2))
+    if sum(spike_counts[:81]) < 20:
+        bin_centers_vals = np.concatenate((-np.flip(bin_centers), bin_centers))
+        stairs_val = np.concatenate((np.flip(spike_counts), spike_counts))
     else:
-        x2_vals = np.concatenate((-np.flip(x2[:1001]), x2[:1001]))
-        stairs_val = np.concatenate((np.flip(number2[:1001]), number2[:1001]))
+        bin_centers_vals = np.concatenate(
+            (-np.flip(bin_centers[:81]), bin_centers[:81])
+        )
+        stairs_val = np.concatenate((np.flip(spike_counts[:81]), spike_counts[:81]))
 
     decimal_points = len(
         str(ref_per).split(".")[1]
     )  # how many decimal places needed to compare to refractory period
-    x2_vals = np.array(
-        [float(f"%.{decimal_points}f" % x) for x in x2_vals]
+    bin_centers_vals = np.array(
+        [float(f"%.{decimal_points}f" % x) for x in bin_centers_vals]
     )  # convert x values to appropriate decimal places
 
-    x2_val_len = int(
-        len(x2_vals) / 8
+    bin_centers_val_len = int(
+        len(bin_centers_vals) / 8
     )  # divide to a small number of values for tick labels
     line2 = np.argwhere(
-        abs(x2_vals) == ref_per
+        abs(bin_centers_vals) == ref_per
     )  # put our lines at refractory period line
 
-    x2_vals = np.array(
-        [float("%.3f" % x) for x in x2_vals]
+    bin_centers_vals = np.array(
+        [float("%.3f" % x) for x in bin_centers_vals]
     )  # convert x-values to 3 decimal points for viusalization
 
     """Make our figure with refractory period lines and stairs. High resolution"""
@@ -99,16 +90,16 @@ def acg(st: np.array, sample_rate: float, cluster: int, ref_per: float) -> None:
         [line2[-1], line2[-1]], [0, np.max(stairs_val) + 5], color="red", linestyle=":"
     )  # refract lines
     ax1.set(
-        xlim=(np.min(x2_vals), np.max(x2_vals)),
-        xticks=np.linspace(0, len(x2_vals), 9),
+        xlim=(np.min(bin_centers_vals), np.max(bin_centers_vals)),
+        xticks=np.linspace(0, len(bin_centers_vals), 9),
         ylabel="Spike Counts",
         xlabel="Time (s)",
     )
-    ax1.set_xticklabels(x2_vals[0:-1:x2_val_len])  # labels a subset
+    ax1.set_xticklabels(bin_centers_vals[0:-1:bin_centers_val_len])  # labels a subset
     plt.rc("axes", labelsize=14)
     plt.rc("xtick", labelsize=12)
     plt.tight_layout()
     sns.despine()  # I like despined figures
-    plt.title("ACG Neuron: {}".format(cluster), fontsize=18, weight="bold")
+    plt.title("ACG Neuron: {}".format(cluster), fontsize=8, weight="bold")
     plt.figure(dpi=1200)
     plt.show()
