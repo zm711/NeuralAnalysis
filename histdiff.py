bf6bb5547b16987872b4cc3f44c736434e737f36
zm711 <92116279+zm711@users.noreply.github.com>
2023-02-16 21:40:29 -0500

    Update histdiff.py
    got the jit-ed version of the python code. Need to speed test, but should be pretty quick.

--- histdiff.py
+++ histdiff.py
@@ -50,20 +50,21 @@ def rusthist(timeStamps, RefPts, binBorders):
     reghistpy(timeStamps, RefPts, binBorders)
 
 
-def histdiff(timeStamps: np.array, referencePoints: np.array, binBorders: np.array):
+@jit(nopython=True)
+def histdiff(time_stamps: np.array, reference_points: np.array, bin_borders: np.array):
     """First we pull in our data"""
-    data1 = timeStamps
-    ndata1 = np.size(timeStamps)
-    data2 = referencePoints
-    ndata2 = np.size(referencePoints)
-    nbins = len(binBorders)
+    data1 = time_stamps
+    ndata1 = len(time_stamps)
+    data2 = reference_points
+    ndata2 = len(reference_points)
+    nbins = len(bin_borders)
     """Now we check if nbins is scalar or vector"""
     if nbins == 1:
         minV, maxV = findext(data1, ndata1, data2, ndata2)
         size = (maxV - minV) / nbins
     else:  # if a vector subtract one to account for the extra "end point"
-        nbins = len(binBorders) - 1
-        bins = binBorders  # all of our bins are listsed in binBorders
+        nbins = len(bin_borders) - 1
+        bins = bin_borders  # all of our bins are listsed in binBorders
         size = bins[1] - bins[0]
         for count in range(1, nbins):
             if (
@@ -73,25 +74,25 @@ def histdiff(timeStamps: np.array, referencePoints: np.array, binBorders: np.arr
                 break
         if size:
             minV = bins[0]
-    """Here we check for which algorithm to use. If size means regularly spaced and we 
-       can use faster if ordered and fast if unordered.
-       Should be ordered for us"""
+    """Here we check for which algorithm to use. If size means regularly spaced 
+     and we can use faster if ordered and fast if unordered. Should be ordered 
+     for us"""
     if size:
         if chckord(data1, ndata1, data2, ndata2):  # check if ordered
             cnts, ctrs = ordhist(data1, ndata1, data2, ndata2, minV, size, nbins)
-            return cnts, ctrs, 1
+            return cnts, ctrs
         else:
             cnts, ctrs = reghist(data1, ndata1, data2, ndata2, minV, size, nbins)
-            return cnts, ctrs, 2
+            return cnts, ctrs
     else:  # if unordered do slow algorithm
         cnts, ctrs = binhist(data1, ndata1, data2, ndata2, bins, nbins)
-        return cnts, ctrs, 3
+        return cnts, ctrs
 
 
 """Sorting algorithms below"""
 
 
-@jit(nopython=True, cache=True)
+@jit(nopython=True)
 def reghist(
     data1: np.array,
     ndata1: int,
@@ -109,26 +110,17 @@ def reghist(
         ctrs[counta] = minV + counta * size + size / 2
 
     for counts in range(ndata1):
-        if ndata2 == 1:
-            diff: float = data1[counts] - data2
+        for counts2 in range(ndata2):
+            diff: float = data1[counts] - data2[counts2]
             if diff < minV or diff >= maxV:
                 continue
             idx = (diff - minV) / size
             cnts[int(idx)] += 1
-        else:
-            for counts2 in range(ndata2):
-                diff = data1[counts] - data2[counts2]
-                if diff < minV or diff >= maxV:
-                    continue
 
-                idx = diff - minV
-                idx = idx / size
-                print(type(idx))
-                cnts[int(idx)] += 1
     return cnts, ctrs
 
 
-@jit(nopython=True, cache=True)
+@jit(nopython=True)
 def ordhist(
     data1: np.array,
     ndata1: int,
@@ -146,25 +138,18 @@ def ordhist(
         ctrs[counta] = minV + counta * size + size / 2
     jmin = 0
     for count_data1 in range(ndata1):
-        if ndata2 == 1:
-            if data1[count_data1] - data2 >= maxV:
-                continue
-            else:
-                diff = data1[count_data1] - data2
+        for counts2 in range(jmin, ndata2):
+            if (data1[count_data1] - data2[counts2]) >= maxV:
+                jmin = counts2
+            for counts3 in range(jmin, ndata2):
+                diff = data1[count_data1] - data2[counts3]
                 if diff >= minV:
                     cnts[int((diff - minV) / size)] += 1
-        else:
-            for counts2 in range(jmin, ndata2):
-                if (data1[count_data1] - data2[counts2]) >= maxV:
-                    jmin = counts2
-                for counts3 in range(jmin, ndata2):
-                    diff = data1[count_data1] - data2[counts3]
-                    if diff >= minV:
-                        cnts[((diff - minV) / size).astype(int)] += 1
+
     return cnts, ctrs
 
 
-@jit(nopython=True, cache=True)
+@jit(nopython=True)
 def binhist(
     data1: np.array,
     ndata1: int,
@@ -191,7 +176,7 @@ def binhist(
 """Auxiliary functions for the finding min and max if unordered data"""
 
 
-@jit(nopython=True, cache=True)
+@jit(nopython=True)
 def findext(
     data1: np.array, ndata1: int, data2: np.array, ndata2: int
 ) -> tuple[float, float]:
@@ -210,7 +195,7 @@ def findext(
 """Auxiliary function for checking if data is ordered or unordered"""
 
 
-@jit(nopython=True, cache=True)
+@jit(nopython=True)
 def chckord(data1: np.array, ndata1: int, data2: np.array, ndata2: int) -> int:
     for counts in range(1, ndata1):
         if data1[counts] < data1[counts - 1]:
