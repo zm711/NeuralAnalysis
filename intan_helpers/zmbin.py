f9d7ceafb53f2421a5b741c4a93462db1a188c6c
zm711 <92116279+zm711@users.noreply.github.com>
2023-02-20 16:51:41 -0500

    Update zmbin.py

--- intan_helpers/zmbin.py
+++ intan_helpers/zmbin.py
@@ -8,7 +8,7 @@ Created on Tue Jun 14 09:57:57 2022
 """
 import numpy as np
 from intan_helpers import load_intan_rhd_format
-from genhelpers import getdir
+from misc.genhelpers import getdir
 import os
 import glob
 
