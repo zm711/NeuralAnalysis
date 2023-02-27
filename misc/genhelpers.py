319f5dca74996a4fe83ac302613a12bb76ef5be2
Zach McKenzie <92116279+zm711@users.noreply.github.com>
2023-02-27 14:13:12 -0500

    Update genhelpers.py
    realized that pyanalysis will never exist.

--- misc/genhelpers.py
+++ misc/genhelpers.py
@@ -92,10 +92,6 @@ to the workspace."""
 
 
 def loadvalues() -> namedtuple:
-    currPath = os.getcwd()
-    if "pyanalysis" not in currPath:
-        os.chdir("pyanalysis")
-        print("collecting file info")
 
     filename = glob.glob("*intan.npy")
     IntanValues = namedtuple(
