eef127b467b7626d9724ba24f200a3f6404a9632
zm711 <92116279+zm711@users.noreply.github.com>
2023-03-09 12:17:55 -0500

    Update __init__.py
    make it check for the config_file and if it doesn't exist it creates it.

--- neuralanalysis/__init__.py
+++ neuralanalysis/__init__.py
@@ -1,1 +1,16 @@
+import yaml
+from pathlib import Path
 
+config_file = Path('./na_settings.yaml')
+
+if config_file.is_file():
+    pass
+else:
+    defaults =[
+    {'zscore': {'inhib': [-2, 3], 'sustained': [3.3, 5], 'onset': [4, 3], 'offset': [2.5, 3]}},
+    {'raw': {'sustained': [75]}},
+    {'sorter_dict': {'Sustained': [50, 100], 'Onset': [50, 65], 'Onset-Offset': [50, 65, 90, 110], 'Relief': [100, 150], ' Inhib': [50, 67]}}
+    ]
+
+    with open('na_settings.yaml', 'w') as f:
+        yaml.dump(defaults, f)
