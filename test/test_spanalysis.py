a77312a6c12f264aef606c9cbcdaf02ae387ca97
zm711 <92116279+zm711@users.noreply.github.com>
2023-03-13 18:17:16 -0400

    Update test_spanalysis.py
    added additional asserts.

--- test/test_spanalysis.py
+++ test/test_spanalysis.py
@@ -5,15 +5,21 @@ Created on Fri Mar 10 13:11:09 2023
 @author: ZacharyMcKenzie
 """
 
-import numpy as np
-from neuralanalysis.analysis import latency_calculator
-from neuralanalysis.analysis import psthfunctions
+
 from test.test_clusterAnalysis import gen_data
 
 from neuralanalysis import SPAnalysis
 
 
 def test_spa():
+    sp, _ = gen_data(123456789)
     spikes = SPAnalysis.SPAnalysis()
 
     assert spikes
+
+    spikes.sp = sp
+    assert spikes.sp
+
+    assert spikes.get_waveforms
+    assert spikes.plot_drift
+    assert spikes.plot_cdf
