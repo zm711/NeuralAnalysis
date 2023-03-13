b398aa4a18d487ea942655f03857a99779812712
zm711 <92116279+zm711@users.noreply.github.com>
2023-03-13 16:18:52 -0400

    Update test_qcfn.py
    added tests

--- test/test_qcfn.py
+++ test/test_qcfn.py
@@ -33,6 +33,27 @@ def test_isisv():
     assert np.isclose(fp, 0.27660304568527916)
 
 
+def test_count_unique():
+    seq = RandomState(1234567890)
+    timestamps = np.array(seq.randint(0, 1000, size=500))
+    values, instances = qcfns.count_unique(timestamps)
+
+    assert len(values) == 409
+    assert len(values) == len(instances)
+
+    assert values[45] == 129
+    assert instances[45] == 1
+
+
+def test_count_unique_simple():
+    test_array = np.array([1, 1, 2, 3, 4, 5, 5, 5])
+    values, instance = qcfns.count_unique(test_array)
+
+    assert len(values) == 5
+    numpy.testing.assert_allclose(np.array(values), np.array([1, 2, 3, 4, 5]))
+    numpy.testing.assert_allclose(np.array(instance), np.array([2, 1, 1, 1, 3]))
+
+
 def test_tipping_point():
     x = np.array([0.0, 1, 3, 4, 5, 9, 10, 16, 17, 18])
     y = np.array(
@@ -50,21 +71,21 @@ def test_tipping_point():
     )
 
     final_pos = qcfns.tipping_point(x, y)
-    assert final_pos == 7
+    assert final_pos == 6
 
 
 def test_tipping_point_simple():
     x = np.array([1, 4, 5])
     y = np.array([2, 3, 6])
     final_pos = qcfns.tipping_point(x, y)
-    assert final_pos == 3
+    assert final_pos == 2
 
 
 def test_tipping_point_simple_low_tip():
     x = np.array([1, 7, 8, 9, 10])
     y = np.array([2, 3, 4, 5, 6, 11])
     pos = qcfns.tipping_point(x, y)
-    assert pos == 2
+    assert pos == 1
 
 
 def test_masked_cluster_quality_core():
@@ -94,24 +115,3 @@ def test_masked_cluster_quality_core():
         this_cluster_fail2, that_cluster
     )
     assert np.isnan(contam_rate)
-
-
-def test_count_unique():
-    seq = RandomState(1234567890)
-    timestamps = np.array(seq.randint(0, 1000, size=500))
-    values, instances = qcfns.count_unique(timestamps)
-
-    assert len(values) == 409
-    assert len(values) == len(instances)
-
-    assert values[45] == 129
-    assert instances[45] == 1
-
-
-def test_count_unique_simple():
-    test_array = np.array([1, 1, 2, 3, 4, 5, 5, 5])
-    values, instance = qcfns.count_unique(test_array)
-
-    assert len(values) == 5
-    numpy.testing.assert_allclose(np.array(values), np.array([1, 2, 3, 4, 5]))
-    numpy.testing.assert_allclose(np.array(instance), np.array([2, 1, 1, 1, 3]))
