09d07736fdf84c46bde25dac7f68b64b3e5f5f41
zm711 <92116279+zm711@users.noreply.github.com>
2023-03-13 17:04:54 -0400

    Update test_paramread.py
    add a cgsread file test

--- test/test_paramread.py
+++ test/test_paramread.py
@@ -6,10 +6,25 @@ Created on Fri Mar 10 18:25:22 2023
 """
 
 import numpy as np
+import numpy.testing
 from neuralanalysis.intan_helpers import stimulushelpers as sh
+
+from neuralanalysis.analysis import spsetup as spset
 
 
 def test_paramread():
+    """test for obtaining sample rate from the params.py file"""
     frequency = sh.paramread()
 
     assert frequency == 30_000
+
+
+def test_read_cgs():
+    """test for reading cgs files for setting up sp"""
+    cgsfile = ["cluster_id	group", "0    mua", "1    good", "2    noise"]
+
+    cids, cgs = spset.readCGSfile(cgsfile)
+
+    assert len(cids) == len(cgs)
+    numpy.testing.assert_allclose(np.array(cids), np.array([0, 1, 2]))
+    numpy.testing.assert_allclose(np.array(cgs), np.array([1, 2, 0]))
