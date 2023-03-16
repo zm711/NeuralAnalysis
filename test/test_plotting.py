b85cfdb50e73759359d02988511d9bef0f12684a
zm711 <92116279+zm711@users.noreply.github.com>
2023-03-16 15:30:22 -0400

    Create test_plotting.py
    add mock tests for plotting functions in api

---
+++ test/test_plotting.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 15:04:24 2023
+
+@author: ZacharyMcKenzie
+"""
+from neuralanalysis.ClusterAnalysis import ClusterAnalysis
+from test.test_clusterAnalysis import gen_data
+
+
+def test_plot_waveforms(mocker):
+    mocker.patch.object(ClusterAnalysis, 'plot_wfs', return_value=1)
+
+    sp, eventTimes = gen_data(1234567890)
+
+    my_neuron = ClusterAnalysis(sp, eventTimes)
+    my_neuron.wf = {"F": "test"}
+
+    assert my_neuron.plot_wfs(ind=True)==1
+
+def test_plot_psth_viewer(mocker):
+    mocker.patch.object(ClusterAnalysis, 'plot_spikes', return_value =1)
+
+    sp, eventTimes = gen_data(1234567890)
+    my_neuron = ClusterAnalysis(sp, eventTimes)
+    assert my_neuron.plot_spikes()==1
+
+def test_acg(mocker):
+    mocker.patch.object(ClusterAnalysis, 'acg', return_value =1)
+
+    sp, eventTimes = gen_data(1234567890)
+    my_neuron = ClusterAnalysis(sp, eventTimes)
+    assert my_neuron.acg()==1
+
+
+def test_plot_fr(mocker):
+    mocker.patch.object(ClusterAnalysis, 'plot_firingrate', return_value =1)
+
+    sp, eventTimes = gen_data(1234567890)
+    my_neuron = ClusterAnalysis(sp, eventTimes)
+    assert my_neuron.plot_firingrate()==1
+
+def test_plot_CDFs(mocker):
+    mocker.patch.object(ClusterAnalysis, 'plot_cdf', return_value =1)
+
+    sp, eventTimes = gen_data(1234567890)
+    my_neuron = ClusterAnalysis(sp, eventTimes)
+    assert my_neuron.plot_cdf()==1
+
+def test_plot_pc(mocker):
+    mocker.patch.object(ClusterAnalysis, 'plot_pc', return_value =1)
+
+    sp, eventTimes = gen_data(1234567890)
+    my_neuron = ClusterAnalysis(sp, eventTimes)
+    assert my_neuron.plot_pc()==1
