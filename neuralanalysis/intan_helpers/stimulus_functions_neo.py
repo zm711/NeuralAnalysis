50d4e0b277f3c0b54e021409883df6bc82b0d0c3
Zach McKenzie <92116279+zm711@users.noreply.github.com>
2023-04-12 15:54:49 -0400

    Update stimulus_functions_neo.py
    fix for final adc if included

--- neuralanalysis/intan_helpers/stimulus_functions_neo.py
+++ neuralanalysis/intan_helpers/stimulus_functions_neo.py
@@ -20,20 +20,23 @@ def process_stim(filename: str = "") -> None:
         filename = filename + ".rhd"
 
     assert ".rhd" in filename, "please make sure the filename given is an .rhd"
-
+    
     final_adc, digital_data, sample_freq = read_intan_neo(filename)
+    
+    intan_dict=dict(frequency_parameters=dict(amplifier_sample_rate=sample_freq))
+    
+    if np.isnan(digital_data, where=False):
 
-    value_matrix, values = preprocess_digital(digital_data)
-    dig_channels = {}
-    for idx, value in enumerate(values):
-        dig_channels[idx] = {"native_channel_name": "DIG" + str(value)}
+        value_matrix, values = preprocess_digital(digital_data)
+        dig_channels = {}
+        for idx, value in enumerate(values):
+            dig_channels[idx] = {"native_channel_name": "DIG" + str(value)}
+        intan_dict['board_dig_in_data'] = value_matrix
+        intan_dict['board_dig_in_channels'] = dig_channels
+        
+    if np.isnan(final_adc, where=False):
+        intan_dict['board_adc_data']=final_adc
 
-    intan_dict = {
-        "board_adc_data": final_adc,
-        "board_dig_in_data": value_matrix,
-        "board_dig_in_channels": dig_channels,
-        "frequency_parameters": {"amplifier_sample_rate": sample_freq},
-    }
     os.mkdir("pyanalysis")
     os.chdir("pyanalysis")
     np.save(filename + ".intan.npy", intan_dict, allow_pickle=True)
@@ -49,26 +52,31 @@ def read_intan_neo(filename: str) -> tuple[np.array, np.array, float]:
     for value in reader.header["signal_streams"]:
         stream_list.append(str(value[0]))
 
-    adc_stream = [idx for idx, name in enumerate(stream_list) if "ADC" in name.upper()][
-        0
-    ]
+    adc_stream = [idx for idx, name in enumerate(stream_list) if "ADC" in name.upper()]
 
     digital_stream = [
         idx for idx, name in enumerate(stream_list) if "DIGITAL-IN" in name.upper()
     ]
-
-    adc_data = reader.get_analogsignal_chunk(
-        stream_index=adc_stream, channel_indexes=[0]
-    )
-
-    final_adc = np.squeeze(
-        reader.rescale_signal_raw_to_float(
-            adc_data, stream_index=adc_stream, dtype="float64"
+    
+    if len(adc_stream)!=0:
+        adc_stream = adc_stream[0]
+        adc_data = reader.get_analogsignal_chunk(
+            stream_index=adc_stream, channel_indexes=[0]
         )
-    )
+
+        final_adc = np.squeeze(
+            reader.rescale_signal_raw_to_float(
+                adc_data, stream_index=adc_stream, dtype="float64"
+            )
+        )
+    else:
+       final_adc=np.nan
 
     if len(digital_stream) == 0:
-        digital_data = intan_neo_read_no_dig(reader)
+        try:
+            digital_data = intan_neo_read_no_dig(reader)
+        except:
+            digital_data = np.nan
     else:
         digital_stream = digital_stream[0]
         digital_data = np.squeeze(
